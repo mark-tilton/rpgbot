bd6cfbe25a60791a289e944ec2efbadd5eaa3cf8
Mark Tilton <mark.tilton.a@gmail.com>
2024-04-20 23:49:21 -0400

    Add permissions handling for zones

--- src/main.py
+++ src/main.py
@@ -21,41 +21,86 @@ tree = CommandTree(client)
 game = Game()
 
 channel_to_zone: Mapping[int, Zone] = {}
+zone_to_channel: Mapping[int, int] = {}
 
 
 @client.event
 async def on_ready():
     print("Initializing Zones")
     # Find existing zones
+    guild = client.get_guild(guild_id)
+    if guild is None or guild.self_role is None:
+        raise Exception("Invalid Guild")
     found_zones: set[int] = set()
-    for channel in client.get_all_channels():
+    for channel in guild.channels:
         zone = ZONES_BY_NAME.get(channel.name, None)
         if zone is not None:
             channel_to_zone[channel.id] = zone
+            zone_to_channel[zone.zone_id] = channel.id
             found_zones.add(zone.zone_id)
+    
+    # Clear out all channels
+    for zone_id in found_zones:
+        channel_id = zone_to_channel[zone_id]
+        channel = guild.get_channel(channel_id)
+        if channel is None or not isinstance(channel, discord.TextChannel):
+            continue
+        await channel.delete()
+    found_zones.clear()
+        
     # Add new zones to the server
+    self_overwrite = discord.PermissionOverwrite()
+    self_overwrite.view_channel = True
+    self_overwrite.manage_channels = True
     for zone in ZONES:
-        if zone.zone_id not in found_zones:
-            hidden_overwrite = discord.PermissionOverwrite()
-            hidden_overwrite.view_channel = zone.public
-            guild = client.get_guild(guild_id)
-            if guild is None:
-                continue
-            channel = await guild.create_text_channel(
-                zone.name,
-                overwrites={
-                    guild.roles[0]: hidden_overwrite
-                }
-            )
-            channel_to_zone[channel.id] = zone
+        if zone.zone_id in found_zones:
+            continue
+        hidden_overwrite = discord.PermissionOverwrite()
+        hidden_overwrite.view_channel = zone.public
+        channel = await guild.create_text_channel(
+            zone.name,
+            overwrites={
+                guild.roles[0]: hidden_overwrite,
+                guild.self_role: self_overwrite,
+            }
+        )
+        channel_to_zone[channel.id] = zone
+        zone_to_channel[zone.zone_id] = channel.id
 
     print(f'We have logged in as {client.user}')
     await tree.sync(guild=discord.Object(id=guild_id))
 
 
+async def handle_adventure_report(
+        interaction: discord.Interaction,
+        report: AdventureReport):
+    # Grant access to channels
+    shown_overwrite = discord.PermissionOverwrite()
+    shown_overwrite.view_channel = True
+    guild = interaction.guild
+    if guild is None:
+        return
+    user = interaction.user
+    if not isinstance(user, discord.Member):
+        return
+    for adventure_step in report.adventure_steps:
+        for zone_id in adventure_step.zones_discovered:
+            channel_id = zone_to_channel[zone_id]
+            channel = guild.get_channel(channel_id)
+            if not isinstance(channel, discord.TextChannel):
+                continue
+            await channel.set_permissions(user, overwrite=shown_overwrite)
+    
+    channel = interaction.channel
+    if not isinstance(channel, discord.TextChannel):
+        return
+    await send_adventure_report(channel, report)
+            
+    
 async def send_adventure_report(
         channel: discord.TextChannel,
         report: AdventureReport):
+    # Send report
     messages: List[List[str]] = []
     report_lines = report.display().splitlines()
     current_message = []
@@ -88,14 +133,13 @@ async def adventure(interaction: discord.Interaction):
     if channel is None:
         return
     zone = channel_to_zone[channel.id]
+
     report = game.start_adventure(user.id, zone.zone_id)
-    channel = interaction.channel
     await interaction.response.send_message(
         f"{name} is adventuring in this area.")
-    if (report is not None
-            and channel is not None
-            and isinstance(channel, discord.TextChannel)):
-        await send_adventure_report(channel, report)
+
+    if report is not None:
+        await handle_adventure_report(interaction, report)
 
 
 @tree.command(
@@ -116,11 +160,8 @@ async def inventory(interaction: discord.Interaction):
     response_string = f"Inventory: \n{item_list}"
     await interaction.response.send_message(response_string, ephemeral=True)
 
-    channel = interaction.channel
-    if (report is not None
-            and channel is not None
-            and isinstance(channel, discord.TextChannel)):
-        await send_adventure_report(channel, report)
+    if report is not None:
+        await handle_adventure_report(interaction, report)
 
 
 @tree.command(
